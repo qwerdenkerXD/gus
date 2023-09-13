a4cfe18b24fab72e211b2055a39cb72b165e356c
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-09-13 10:30:38 +0200

    extended CLI defined commmands: start, create_model

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
+mod cli;
 mod model;
 
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[command(version, about)]
-struct Cli {
-    #[structopt(short, long, default_value = "./models", help = "The path to the model definitions")]
-    modelspath: String,
+fn main() {
+    let args = cli::Cli::parse();
+    match args.command {
+        cli::Commands::Start(cmd) => start(cmd),
+        cli::Commands::CreateModel(cmd) => create_model(cmd)
+    }
 }
 
-fn main() {
-    let args = Cli::parse();
+fn start(args: cli::StartServer) {
     if let Err(_) = model::parse_models(args.modelspath.clone()) {
         println!("Warning: No models defined in {}", args.modelspath.clone());
     }
+}
+
+fn create_model(args: cli::CreateModel) {
+    unimplemented!()
 }

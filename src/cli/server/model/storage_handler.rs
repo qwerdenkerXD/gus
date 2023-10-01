2b10f22ddd91ea0c2b85f89fd433a12d2da39ba4
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-10-01 10:33:41 +0200

    turned enum StorageConfig to struct better for serializing

--- src/cli/server/model/storage_handler.rs
+++ src/cli/server/model/storage_handler.rs
@@ -2,7 +2,6 @@ mod json;
 
 use crate::cli;
 
-use std::collections::HashMap;
 use std::io::{
     ErrorKind,
     Result,
@@ -25,7 +24,6 @@ use super::{
 use std::fs::read_to_string;
 use serde_json::from_str;
 
-
 #[allow(non_camel_case_types)]
 #[derive(Deserialize, Serialize, Debug, PartialEq, Hash, Eq, Clone)]
 // if adding storage types, add them in the create-model dialogue in cli.rs too
@@ -33,12 +31,9 @@ pub enum StorageType {
     json
 }
 
-type Storages = HashMap<StorageType, Option<StorageConfig>>;
-
 #[derive(Deserialize, Serialize, Debug, Clone)]
-#[serde(untagged)]
-enum StorageConfig {
-    Json(JsonStorageConfig)
+struct StorageConfig {
+    json: Option<JsonStorageConfig>
 }
 
 pub trait StorageHandler {
@@ -49,41 +44,45 @@ pub trait StorageHandler {
 }
 
 pub fn get_handler(storage_type: &StorageType, model_name: &ModelName) -> Result<impl StorageHandler> {
-    let storage_config: StorageConfig = get_storage_config(storage_type)?;
-    match storage_config {
-        StorageConfig::Json(config) =>
+    let storage_config: StorageConfig = get_storage_configs()?;
+    match storage_type {
+        StorageType::json =>
             Ok(
                 JsonStorageHandler {
                     model_name: model_name.clone(),
-                    config: config
+                    config: storage_config.json.unwrap()
                 }
             ),
     }
 }
 
-fn get_storage_config(storage_type: &StorageType) -> Result<StorageConfig> {
+fn get_storage_configs() -> Result<StorageConfig> {
+    let mut storage_configs = StorageConfig {
+        json: None
+    };
     if let Some(start) = cli::get_valid_start_args() {
         if let Some(path_buf) = start.storage_definitions {
             let data: Result<String> = read_to_string(&path_buf.as_path());
             if data.is_err() {
                 return Err(Error::new(ErrorKind::PermissionDenied, "Unable to read storage definition file, make sure it's utf-8 only"))
             }
-            if let Ok(storages) = from_str::<Storages>(&data.unwrap()) {
-                if let Some(storage) = storages.get(storage_type) {
-                    if let Some(config) = storage {
-                        return Ok(config.clone());
-                    }
-                }
+            if let Ok(storages) = from_str::<StorageConfig>(&data.unwrap()) {
+                storage_configs = storages;
             } else {
                 return Err(Error::new(ErrorKind::InvalidData, "The storage definition file is invalid"))
             }
         }
 
-        match storage_type {
-            StorageType::json => return Ok(StorageConfig::Json(JsonStorageConfig {
-                storage_file: None
-            }))
+        // set defaults if None
+        if storage_configs.json.is_none() {
+            storage_configs.json = Some(
+                JsonStorageConfig {
+                    storage_file: None
+                }
+            );
         }
+
+        return Ok(storage_configs);
     }
     todo!("getting storage configs is currently only possible when the server is running")
 }

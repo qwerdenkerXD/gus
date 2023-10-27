50557d1daedf1be4248eb71d1779b53f60994ce3
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-10-27 20:51:00 +0200

    extended graphql.rs implemented get_executing_operation

--- src/cli/server/graphql.rs
+++ src/cli/server/graphql.rs
@@ -2,8 +2,8 @@
 use std::collections::HashMap;
 use super::model::Record;
 use apollo_parser::cst::{
+    OperationDefinition as Operation,
     FragmentDefinition as Fragment,
-    OperationDefinition as Query,
     CstChildren
 };
 use serde_derive::{
@@ -76,7 +76,7 @@ impl TryFrom<&str> for GraphQLPost {
 }
 
 pub fn handle_post_request(body: GraphQLPost) -> GraphQLReturn {
-    let parser = Parser::new(&body.query);
+    let parser = Parser::new(&body.query); //.token_limit(...).recursion_limit(...) TODO!
     let cst: SyntaxTree = parser.parse();
     if cst.errors().len() > 0 {
         let mut errors: Errors = vec!();
@@ -92,31 +92,57 @@ pub fn handle_post_request(body: GraphQLPost) -> GraphQLReturn {
     let definitions: CstChildren<Definition> = cst.document().definitions();
 
     let mut fragments = vec!();
-    let mut queries = vec!();
+    let mut operations = vec!();
 
     for def in definitions {
         match def {
             Definition::FragmentDefinition(fragment) => fragments.push(fragment),
-            Definition::OperationDefinition(query) => queries.push(query),
+            Definition::OperationDefinition(op) => operations.push(op),
             _ => ()
         }
     }
 
-    handle_queries(Queries{
-        fragments,
-        queries
-    })
+    let exec_operation: Operation;
+    match get_executing_operation(operations, body.operationName) {
+        Ok(op) => exec_operation = op,
+        Err(ret) => return ret
+    }
+
+    todo!()
 }
 
 pub fn handle_get_request(query: GraphQLGet) -> GraphQLReturn {
     handle_post_request(GraphQLPost::from(query))
 }
 
-struct Queries {
-    fragments: Vec<Fragment>,
-    queries: Vec<Query>
-}
-
-fn handle_queries(queries: Queries) -> GraphQLReturn {
-    todo!()
+fn get_executing_operation(operations: Vec<Operation>, operation_name: Option<String>) -> Result<Operation, GraphQLReturn> {
+    let mut ops: Vec<Operation> = operations;
+    match ops.len() {
+        0 => Err(GraphQLReturn {
+            errors: Some(vec!("GraphQL Error: Document does not contain any operations".to_string())),
+            data: None
+        }),
+        1 => Ok(ops.pop().unwrap()),
+        len => {
+            if operation_name.is_none() {
+                return Err(GraphQLReturn {
+                    errors: Some(vec!("GraphQL Error: Document contains more than one operation, missing operation name".to_string())),
+                    data: None
+                });
+            }
+            ops.retain(|o| {
+                match o.name() {
+                    Some(name) => &name.text() == operation_name.as_ref().unwrap(),
+                    None => false
+                }
+            });
+            match ops.pop() {
+                Some(o) => Ok(o),
+                None => Err(GraphQLReturn {
+                    errors: Some(vec!(format!("GraphQL Error: Operation with name {} does not exist", operation_name.unwrap().as_str()))),
+                    data: None
+                })
+            }
+        }
+    }
 }
